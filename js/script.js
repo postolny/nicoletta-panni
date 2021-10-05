a12999c7258f1e717079d42479e3a629dc16a440
Igor Postolny <74405055+postolny@users.noreply.github.com>
2021-10-05 19:00:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  var controls = {
+    audio: $("#musica"),
+    playpause: $("#playpause")
+  };
+  var audio = controls.audio[0];
+  controls.playpause.click(function() {
+    if (audio.paused) {
+      audio.play();
+      $(this).addClass("start");
+    } else {
+      audio.pause();
+      $(this).removeClass("start");
+      audio.currentTime = 0;
+    }
+  });
+  $(audio).on('ended', function() {
+    audio.pause();
+    controls.playpause.addClass("stop");
+    controls.playpause.toggleClass("start");
+    $('.modal').css({ 'opacity': '1', 'visibility': 'visible' });
+  });
+  $('.modal_close,.modal_bg').click(function() {
+    $('.modal').css({ 'opacity': '0', 'visibility': 'hidden' });
+  });
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) $('.modal').css({ 'opacity': '0', 'visibility': 'hidden' }); // esc
+  });
+  $('#inverno').click(function() {
+    $(this).animate({ bottom: "-155px" }, 700, function() {
+      $('#pupazzo_di_neve').animate({ bottom: "0" }, 500, "easeOutBounce");
+    });
+  });
+  $('#pupazzo_di_neve').click(function() {
+    $('#inverno').animate({ bottom: "0" }, 700);
+    $(this).animate({ bottom: "-96px" }, 700);
+  });
+  var year = function() {
+    return new Date().getFullYear();
+  };
+  $('.year').text(year());
+});
